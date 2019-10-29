32c8d9b14ef02e7be3d08457848dc41094535923
Masumi.M <masumi.lacrosse@live.jp>
2019-10-29 19:10:15 +0900

    Edit: ScrapeImage

--- ScrapeImage.sh
+++ ScrapeImage.sh
@@ -3,15 +3,10 @@
 # ===== Memo =====
 # Setup1: Give Permission by `chmod 755 ScrapeImage.sh`
 # Setup2: Install wget before execution (`brew install wget`)
+# Info: Scrapes the image from the browser and printout.
 
-# Info
-# 1. Scrapes the image from the browser.
-# 2. Open the image that was scraped.
-# 3. Print out image.
-
-# Setpatameter
 currentPath="`pwd`"
-downloadLink="https://www.umi-mori.jp/Resource/img/profile.png" # edit the browser path
+downloadLink="https://www.umi-mori.jp/Resource/img/profile.png"
 wget -v $downloadLink
 
 echo "===== start ====="
